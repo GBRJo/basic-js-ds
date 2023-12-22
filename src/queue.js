680a381d163899955d3a2839e47aceb2d25289e6
GBRJo <anchernyakova@yandex.ru>
2023-12-22 06:48:46 +0400

    feat: add other exercises

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.head = null; 
+    this.tail = null;
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const { ListNode } = require('../extensions/list-node.js')
+    const newNode = new ListNode(value);
+  if (!this.head) {
+    this.head = newNode;
+  } else {
+    this.tail.next = newNode;
   }
+  this.tail = newNode;
+}
+  
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head) {
+      return null;
+    }
+  
+    const currentValue = this.head.value;
+    this.head = this.head.next; 
+    if (!this.head) { 
+      this.tail = null;
+    }
+    return currentValue;
   }
 }
 
